027506a3916729948c4fb81c1d0771bc74387466
Kevin <Kevinaichinger1@gmail.com>
2021-11-22 11:14:38 +0100

    angular detailView

--- www/src/app/app-routing.module.ts
+++ www/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ItemDetailComponent } from './item-detailTable/item-detail/item-detail.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 import { ItemTableComponent } from './item-table/item-table.component';
 
 const routes: Routes = [ 
